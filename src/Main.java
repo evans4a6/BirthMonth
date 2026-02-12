3e48deb2a1cdf8a522797193080201407a26f62f
addisonevans <evans4a6@mail.uc.edu>
2026-02-12 18:10:43 -0500

    Lab 5, Task 2 Complete

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,21 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner in = new Scanner(System.in);
+        int birthMonth = 0;
+        String enterPlease = "What is your birth month [1-12]?: ";
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.print(enterPlease);
+        birthMonth = in.nextInt();
+
+        if (birthMonth <= 12 && birthMonth >= 1) {
+            System.out.println("Your birth month is " + birthMonth);
+        } else {
+            System.out.println("\nYou entered an invalid month of " + birthMonth);
+            System.out.println("Please try the program again");
         }
+
     }
 }
